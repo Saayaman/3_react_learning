3f58711b61ee36bddde4c4f05060018f3da03b30
Saayaman <ayakosayama1987@gmail.com>
2018-06-16 01:23:15 -0700

    3-1. 親Componentに追加されたTodoのデータを送る

--- todo-app/src/components/AddTodo.jsx
+++ todo-app/src/components/AddTodo.jsx
@@ -8,7 +8,7 @@ export default class AddTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.inputValue);
+    this.props.addTodo(this.state.inputValue);
   }
 
   handleChange = (e) => {
