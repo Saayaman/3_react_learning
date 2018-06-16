000d11ba52ae42e05944d760e7fa21d554f5195c
Saayaman <ayakosayama1987@gmail.com>
2018-06-16 00:34:27 -0700

    2-2. Footer MenuにUIを足す

--- todo-app/src/components/FooterMenu.jsx
+++ todo-app/src/components/FooterMenu.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
 
 export default class FooterMenu extends Component {
+
+  state = {
+    activeFilter: 'all',
+  };
+
+  handleFilter = (filter) => {
+    this.setState({
+      activeFilter: filter,
+    });
+
+    //todo: ここにアクションを書く
+  }
+
   render() {
+
+    const { activeFilter } = this.state;
+
     return (
       <div>
-        FooterMenu
-      </div>
+        <span>Show: </span>
+        <button disabled={activeFilter === 'all'}
+          onClick={() => this.handleFilter('all')}
+        >
+          All
+        </button>
+        <button 
+          disabled={activeFilter === 'active'}
+          onClick={() => this.handleFilter('active')}
+        >
+          Active
+        </button>
+        <button
+          disabled={activeFilter === 'completed'}
+          onClick={() => this.handleFilter('completed')}
+        >
+          Completed
+        </button>
+    </div>
     )
   }
 }
