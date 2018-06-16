cec575ce802fa6e73ddba1cca9692184789bf4db
Saayaman <ayakosayama1987@gmail.com>
2018-06-15 22:30:03 -0700

    2. とりあえず、3つのComponentを作る

---
+++ todo-app/src/components/FooterMenu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FooterMenu extends Component {
+  render() {
+    return (
+      <div>
+        FooterMenu
+      </div>
+    )
+  }
+}
